00577c7367d5e180ddbdc79562f032fcbb5d068b
Tyler <tylershelton99@gmail.com>
2021-07-06 01:08:47 +0000

    Add tinkering.js to git repo as well

---
+++ tinkering.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+const result = multiply(102, 41);
+
+const result2 = subtract(54, 23);
+
+console.log(result);
+console.log(result2);
