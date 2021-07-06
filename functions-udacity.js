282fd097db64e5fcbd82eb5a32b0c6ec6bd6da9c
Tyler <tylershelton99@gmail.com>
2021-07-06 05:01:11 +0000

    Completed Quiz 4: Laugh in Module 5 - Functions on Udacity

--- functions-udacity.js
+++ functions-udacity.js
@@ -42,22 +42,69 @@
 
 // var result = addTen(2);
 // console.log(divideByThree(result));
-function makeLine(length) {
-  var line = "";
-  for (var j = 1; j <= length; j++) {
-    line += "* "
+
+// function makeLine(length) {
+//   var line = "";
+//   for (var j = 1; j <= length; j++) {
+//     line += "* "
+//   }
+//   return line + "\n";
+// }
+
+// function buildTriangle(baseWidth) {
+//   let triangle = [];  // empty array to store the rows of stars for the triangle
+
+//   for (triangleRow = 1; triangleRow <= baseWidth; triangleRow++) {
+//     triangle += makeLine(triangleRow);  // add each row of stars to build the triangle
+//   }
+//   return triangle;
+
+// }
+
+// console.log(buildTriangle(10));
+
+// var catSays = function (max) {
+//   var catMessage = "";
+//   for (var i = 0; i < max; i++) {
+//     catMessage += "meow ";
+//   }
+//   return catMessage;
+// };
+
+// console.log(catSays);
+
+// // Function expression that assigns the function displayFavorite 
+// // to the variable favoriteMovie
+// var favoriteMovie = function displayFavorite(movieName) {
+//   console.log("My favorite movie is " + movieName);
+// };
+
+// // Function declaration that has two parameters: a function for displaying
+// // a message, along with a name of a movie
+// function movies(messageFunction, name) {
+//   messageFunction(name);
+// }
+
+// // Call the movies function, pass in the favoriteMovie function and name of movie
+// movies(favoriteMovie, "Finding Nemo");
+
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+// function games(messageFunction, name) {
+//   messageFunction(name);
+// }
+
+// // Call the movies function, pass in the function and name of movie
+// games(function displayFavorite(gameName) {
+//   console.log("My favorite video game is " + gameName);
+// }, "Days Gone");
+
+var laugh = function (num) {
+  let laughs = "";
+  for (i = 1; i <= num; i++) {
+    laughs += "ha";
   }
-  return line + "\n";
+  laughs += "!";
+  return laughs;
 }
-
-function buildTriangle(baseWidth) {
-  let triangle = [];
-
-  for (triangleRow = 1; triangleRow <= baseWidth; triangleRow++) {
-    triangle += makeLine(triangleRow);
-  }
-  return triangle;
-
-}
-
-console.log(buildTriangle(10));
+console.log(laugh(10));
